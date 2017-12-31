1614cbb3d6242cb6bbd47674563acc6ad2477d4b
liaoliang <liao0007@gmail.com>
2017-12-31 14:59:28 +0800

    up

--- src/modules/biz/kfc/router.js
+++ src/modules/biz/kfc/router.js
@@ -5,15 +5,15 @@ import dynamic from 'dva/dynamic';
 export default ({ app, history, match }) => {
 
   const Calendar2018Module = dynamic({ app, component: () => import('./calendar-2018/router'), });
-  // const Christmas20171218Module = dynamic({ app, component: () => import('./christmas-20171218/router'), });
-  // const BelgiumIceCream20171218App = dynamic({ app, component: () => import('./belgium-ice-cream-20171218/router'), });
+  const Christmas20171218Module = dynamic({ app, component: () => import('./christmas-20171218/router'), });
+  const BelgiumIceCream20171218App = dynamic({ app, component: () => import('./belgium-ice-cream-20171218/router'), });
 
   return (
     <Router history={history} >
       <Switch >
         <Route path={`${match.path}/calendar-2018`} render={(props) => <Calendar2018Module {...props} app={app} />} />
-        {/*<Route path={`${match.path}/christmas-20171218`} render={(props) => <Christmas20171218Module {...props} app={app} />} />*/}
-        {/*<Route path={`${match.path}/belgium-ice-cream-20171218`} render={(props) => <BelgiumIceCream20171218App {...props} app={app} />} />*/}
+        <Route path={`${match.path}/christmas-20171218`} render={(props) => <Christmas20171218Module {...props} app={app} />} />
+        <Route path={`${match.path}/belgium-ice-cream-20171218`} render={(props) => <BelgiumIceCream20171218App {...props} app={app} />} />
       </Switch >
     </Router >
   );
