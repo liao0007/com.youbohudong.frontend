c66208488dbc42ffeda34047a5eea43eec4f1115
liaoliang <liao0007@gmail.com>
2017-12-31 15:19:31 +0800

    up

--- src/modules/biz/kfc/calendar-2018/routes/DownloadPage.js
+++ src/modules/biz/kfc/calendar-2018/routes/DownloadPage.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Modal, Button, WhiteSpace, WingBlank, Toast } from 'antd-mobile';
-
-const alert = Modal.alert;
-
 import cover from '../assets/cover.gif';
 import apple from '../assets/apple.png';
 import android from '../assets/android.png';
 import hint from '../assets/hint.gif';
 import coupon from '../assets/coupon.png';
 import { Constant } from '../../../../../constant';
+import DocumentTitle from 'react-document-title';
+
+const alert = Modal.alert;
 
 function isWeChatBrowser() {
   return /micromessenger/.test(navigator.userAgent.toLowerCase());
@@ -59,7 +59,7 @@ class DownloadPage extends Component {
     };
 
     return (
-      <div >
+      <DocumentTitle title="K记大玩家" >
         <div style={{ position: 'relative', backgroundColor: '#000000', overflow: 'hidden', width: window.innerWidth, height: window.innerHeight, }} >
 
           <img src={cover} style={{ position: 'absolute', width: '100%', top: Math.floor((window.innerHeight - 667) / 2), left: 0, }} alt="" />
@@ -78,15 +78,25 @@ class DownloadPage extends Component {
             <img src={hint} style={{ position: 'absolute', right: 20, top: 20, width: '50%', }} alt="" />
           </div >
         </div >
-      </div >
+      </DocumentTitle >
     );
   }
 
   gotoAppStore() {
-    this.handleShowAlert();
-
-    // skip for now
+    alert('敬请期待', <div >App正在审核，敬请期待。安卓已开放，安卓手机扫描二维码即可下载使用！<img src={coupon} style={{ width: '100%' }} /></div >, [
+      {
+        text: '取消',
+        onPress: () => {},
+        style: 'default',
+      },
+      {
+        text: '领取',
+        onPress: () => window.location.href = 'https://mp.weixin.qq.com/bizmall/cardshelf?t=cardticket/shelf_list&biz=MzIwMDAyMDI4MQ==&shelf_id=26&showwxpaytitle=1&scene=1000007#wechat_redirect',
+      },
+    ]);
     return;
+
+    //
     if (isWeChatBrowser()) {
       this.setState({
         isHintVisible: true,
@@ -106,47 +116,18 @@ class DownloadPage extends Component {
     }
   };
 
-  handleShowAlert() {
-    alert('敬请期待', <div >App正在审核，敬请期待。安卓已开放，安卓手机扫描二维码即可下载使用！<img src={coupon} style={{ width: '100%' }} /></div >, [
-      {
-        text: '取消',
-        onPress: () => {},
-        style: 'default',
-      },
-      {
-        text: '领取',
-        onPress: () => window.location.href = 'https://mp.weixin.qq.com/bizmall/cardshelf?t=cardticket/shelf_list&biz=MzIwMDAyMDI4MQ==&shelf_id=26&showwxpaytitle=1&scene=1000007#wechat_redirect',
-      },
-    ]);
-  };
-
   componentDidMount() {
-    document.title = '下载K记大玩家';
-    const mobileOS = getMobileOperatingSystem();
+    switch (getMobileOperatingSystem()) {
+      case 'Android':
+        this.downloadAndroidApk();
+        break;
 
-    if (isWeChatBrowser() && mobileOS === 'Android') {
-      this.setState({
-        isHintVisible: true,
-      });
+      case 'iOS':
+        this.gotoAppStore();
+        break;
 
-    } else if (isWeChatBrowser() && mobileOS === 'iOS') {
-      this.handleShowAlert();
-
-    } else {
-      setTimeout(() => {
-        switch (mobileOS) {
-          case 'Android':
-            this.downloadAndroidApk();
-            break;
-
-          case 'iOS':
-            this.gotoAppStore();
-            break;
-
-          default:
-            return null;
-        }
-      }, 200);
+      default:
+        break;
     }
   }
 }
