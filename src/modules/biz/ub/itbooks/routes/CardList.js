0a5115c0010073df288a622ef8de61c725ec3ca3
liaoliang <liao0007@gmail.com>
2018-01-12 23:26:12 +0800

    up

--- src/modules/biz/ub/itbooks/routes/CardList.js
+++ src/modules/biz/ub/itbooks/routes/CardList.js
@@ -8,6 +8,7 @@ import Ellipsis from 'ant-design-pro/lib/Ellipsis';
 import striptags from 'striptags';
 import Drawer from 'rc-drawer-menu';
 import DescriptionList from '../../../../../components/antd-pro/DescriptionList';
+import { enquireScreen } from 'enquire-js';
 
 const { Description } = DescriptionList;
 
@@ -16,6 +17,7 @@ class CardList extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      isMobile: false,
       showLoadingMore: false,
       activeBook: undefined,
     };
@@ -23,14 +25,23 @@ class CardList extends PureComponent {
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      ...this.state,
       showLoadingMore: nextProps.books.pagination ? nextProps.books.pagination.pager.page < nextProps.books.pagination.totalPages : false,
     });
   }
 
   componentDidMount() {
+    enquireScreen((b) => {
+      this.setState({
+        isMobile: !!b,
+      });
+    });
+
     const { activeCategory, activeSubcategory, keywords } = this.props.match.params;
     this.props.dispatch({ type: 'book/listBook', payload: { activeCategory, activeSubcategory, keywords, page: 1 } });
+  }
+
+  handleDownload(item) {
+    window.open(`http://static.itbooks.youbohudong.com/${item.url}`);
   }
 
   render() {
@@ -55,35 +66,45 @@ class CardList extends PureComponent {
           level={null}
           iconChild={false}
           open={this.state.activeBook !== undefined}
-          // width="100vw"
+          width={this.state.isMobile ? '100vw' : '60vw'}
           placement="right"
+          onMaskClick={() => this.setState({ activeBook: undefined })}
         >
           {this.state.activeBook ? (
-            <Row className={styles.drawerDetail} gutter={24} style={{ margin: '16px 0', color: '#ffffff' }}>
-              <Col span={16}>
-                <h1>{this.state.activeBook.title}</h1>
-                <h2>{this.state.activeBook.subtitle}</h2>
-                <DescriptionList
-                  style={{ marginBottom: 24 }}
-                  col={1}
-                >
-                  <Description term='Author'>{this.state.activeBook.author}</Description>
-                  <Description term="ISBN-10">{this.state.activeBook.isbn}</Description>
-                  <Description term='Year'>{this.state.activeBook.year}</Description>
-                  <Description term="Pages">{this.state.activeBook.pages}</Description>
-                  <Description term='Language'>{this.state.activeBook.language}</Description>
-                  <Description term='File size'>{this.state.activeBook.fileSize}</Description>
-                  <Description term='File format'>{this.state.activeBook.fileFormat}</Description>
-                  <Description term='Category'>{this.state.activeBook.category}</Description>
-                </DescriptionList>
-                <div dangerouslySetInnerHTML={{ __html: this.state.activeBook.description }}/>
-                <Button ghost size={'large'} onClick={() => this.setState({ ...this.state, activeBook: undefined })}
-                        icon={'close'}>关闭</Button>
-              </Col>
-              <Col span={8} style={{ textAlign: 'right' }}>
-                <img style={{width:"100%"}} alt="cover" src={`http://static.itbooks.youbohudong.com/${this.state.activeBook.cover}`}/>
-              </Col>
-            </Row>
+            <div className={styles.drawerDetail} style={{ margin: 24 }}>
+              <Row>
+                <Col span={16}>
+                  <h2>{this.state.activeBook.title}</h2>
+                  <h3>{this.state.activeBook.subtitle}&nbsp;</h3>
+                  <DescriptionList
+                    className={styles.descriptionList}
+                    style={{ marginBottom: 24 }}
+                    col={2}
+                    size={'small'}
+                  >
+                    <Description term='Author'>{this.state.activeBook.author}</Description>
+                    <Description term="ISBN-10">{this.state.activeBook.isbn}</Description>
+                    <Description term='Year'>{this.state.activeBook.year}</Description>
+                    <Description term="Pages">{this.state.activeBook.pages}</Description>
+                    <Description term='Language'>{this.state.activeBook.language}</Description>
+                    <Description term='File size'>{this.state.activeBook.fileSize} MB</Description>
+                    <Description term='File format'>{this.state.activeBook.fileFormat}</Description>
+                  </DescriptionList>
+                </Col>
+                <Col span={8}>
+                  <img style={{ width: '100%' }} alt="cover" src={`http://static.itbooks.youbohudong.com/${this.state.activeBook.cover}`}/>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: 24 }}>
+                <Col span={24}>
+                  <div dangerouslySetInnerHTML={{ __html: this.state.activeBook.description }}/>
+                  <Button ghost style={{ marginRight: 16 }} type={'primary'} size={'large'} onClick={() => this.handleDownload(this.state.activeBook)}
+                          icon={'download'}>下载PDF</Button>
+                  <Button ghost size={'large'} onClick={() => this.setState({ activeBook: undefined })} icon={'close'}>关闭</Button>
+                </Col>
+              </Row>
+            </div>
+
           ) : null}
         </Drawer>
 
@@ -101,12 +122,14 @@ class CardList extends PureComponent {
               <List.Item
                 key={item.uid}
                 actions={[
-                  <a onClick={() => window.open(`http://static.itbooks.youbohudong.com/${item.url}`)}><Icon type="download"
-                                                                                                            style={{ marginRight: 8 }}/>下载PDF</a>,
-                  <a onClick={() => this.setState({ ...this.state, activeBook: item })}><Icon type="info-circle-o"
-                                                                                              style={{ marginRight: 8 }}/>详情</a>,
+                  <a onClick={() => this.handleDownload(item)}><Icon type="download"
+                                                                     style={{ marginRight: 8 }}/>下载PDF</a>,
+                  <a onClick={() => this.setState({ activeBook: item })}><Icon type="info-circle-o"
+                                                                               style={{ marginRight: 8 }}/>详情</a>,
                 ]}
-                extra={<img width={200} alt="cover" src={`http://static.itbooks.youbohudong.com/${item.cover}`}/>}
+                extra={<img style={{ marginBottom: this.state.isMobile ? 16 : 0 }} width={this.state.isMobile ? window.innerWidth - 48 : 200}
+                            alt="cover"
+                            src={`http://static.itbooks.youbohudong.com/${item.cover}`}/>}
               >
 
                 <List.Item.Meta
