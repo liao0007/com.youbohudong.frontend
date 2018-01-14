28b52bb0891721c14483d7ef7828cc31e69c058d
liaoliang <liao0007@gmail.com>
2018-01-15 00:04:45 +0800

    up

--- src/modules/biz/ub/itbooks/services/book.js
+++ src/modules/biz/ub/itbooks/services/book.js
@@ -5,6 +5,5 @@ export async function listBook({ activeCategory, activeSubcategory, keywords, pa
   const subcategoryQueryString = activeSubcategory ? `f.subcategory=${activeSubcategory}` : undefined;
   const keywordsQuery = keywords ? `f.keywords=${keywords.toLowerCase()}` : undefined;
   const filterQuery = [categoryQueryString, subcategoryQueryString, keywordsQuery].filter(item => item !== undefined).join('&');
-  return request(`/api/biz/ub/itbooks/books?p.size=12&p.page=${page}&${filterQuery}`);
+  return request(`/api/biz/ub/itbooks/books?s.att=year&s.ord=desc&p.size=12&p.page=${page}&${filterQuery}`);
 }
-
